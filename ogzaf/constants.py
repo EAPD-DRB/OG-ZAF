28758aa4c55517d1da9157f67ed7459c99e5592e
jdebacker <jason.debacker@gmail.com>
2023-02-13 16:30:25 -0500

    black format

--- ogzaf/constants.py
+++ ogzaf/constants.py
@@ -296,11 +296,11 @@ PARAM_LABELS = {
 # 'constant_rates', 'zero_taxes'
 
 
-'''
+"""
 Create dictionaries to map micro categories to broad groups
-'''
+"""
 cons_dict = {
-    "Food" : [
+    "Food": [
         "cagri",
         "clani",
         "cfore",
@@ -323,14 +323,14 @@ cons_dict = {
         "calcb",
         "csftd",
         "ctoba",
-],
+    ],
     "Energy and extraction": [
         "ccoal",
         "cmore",
         "comin",
         "celcg",
-        ],
-    "Non-durables" : [
+    ],
+    "Non-durables": [
         "ctexf",
         "ctexm",
         "ccarp",
@@ -362,7 +362,7 @@ cons_dict = {
         "cjewl",
         "comnf",
         "cwast",
-],
+    ],
     "Durables": [
         "cirst",
         "cnfme",
@@ -386,7 +386,7 @@ cons_dict = {
         "cairc",
         "coteq",
         "ccnst",
-        ],
+    ],
     "Services": [
         "ccsrv",
         "ctrad",
@@ -413,7 +413,7 @@ cons_dict = {
         "ceduc",
         "cheal",
         "cosrv",
-        ]
+    ],
 }
 prod_dict = {
     "Primary": [
@@ -427,7 +427,7 @@ prod_dict = {
     ],
     "Energy": [
         "aelcg",
-            ],
+    ],
     "Tertiary": [
         "awtrd",
         "artrd",
@@ -454,7 +454,7 @@ prod_dict = {
         "arecr",
         "aoact",
         "anobs",
-        ],
+    ],
     "Secondary Ex Energy": [
         "afood",
         "abevt",
@@ -488,17 +488,23 @@ prod_dict = {
     ],
 }
 
-'''
+"""
 Read in SAM file
-'''
+"""
 # Read in SAM file
-storage_options = {'User-Agent': 'Mozilla/5.0'}
+storage_options = {"User-Agent": "Mozilla/5.0"}
 SAM_path = "https://www.wider.unu.edu/sites/default/files/Data/SASAM-2015-Data-Resource.xlsx"
-SAM = pd.read_excel(SAM_path, sheet_name="Micro SAM 2015", skiprows=6, index_col=0, storage_options=storage_options)
+SAM = pd.read_excel(
+    SAM_path,
+    sheet_name="Micro SAM 2015",
+    skiprows=6,
+    index_col=0,
+    storage_options=storage_options,
+)
 
 
 def get_alpha_c(SAM, cons_dict):
-    '''
+    """
     Calibrate the alpha_c vector, showing the shares of household
     expenditures for each consumption category
 
@@ -508,11 +514,14 @@ def get_alpha_c(SAM, cons_dict):
 
     Returns:
         alpha_c (dict): Dictionary of shares of household expenditures
-    '''
+    """
     overall_sum = 0
     for key, value in cons_dict.items():
         # note the subtraction of the row to focus on domestic consumption
-        categroy_total = SAM.loc[SAM.index.isin(value), "total"].sum() - SAM.loc[SAM.index.isin(value), "row"].sum()
+        categroy_total = (
+            SAM.loc[SAM.index.isin(value), "total"].sum()
+            - SAM.loc[SAM.index.isin(value), "row"].sum()
+        )
         alpha_c[key] = categroy_total
         overall_sum += categroy_total
     for key, value in cons_dict.items():
@@ -522,7 +531,7 @@ def get_alpha_c(SAM, cons_dict):
 
 
 def get_io_matrix(SAM, cons_dict, prod_dict):
-    '''
+    """
     Calibrate the io_matrix array.  This array relates the share of each
     production category in each consumption category
 
@@ -533,14 +542,12 @@ def get_io_matrix(SAM, cons_dict, prod_dict):
 
     Returns:
         io_df (pd.DataFrame): Dataframe of io_matrix
-    '''
+    """
     # Create initial matrix as dataframe of 0's to fill in
     io_dict = {}
     for key in prod_dict.keys():
         io_dict[key] = np.zeros(len(cons_dict.keys()))
-    io_df = pd.DataFrame(
-        io_dict, index=cons_dict.keys()
-    )
+    io_df = pd.DataFrame(io_dict, index=cons_dict.keys())
     # Fill in the matrix
     # Note, each cell in the SAM represents a payment from the columns
     # account to the row account
@@ -549,21 +556,24 @@ def get_io_matrix(SAM, cons_dict, prod_dict):
     # the production categories from columns
     for ck, cv in cons_dict.items():
         for pk, pv in prod_dict.items():
-            io_df.loc[io_df.index == ck, pk] = SAM.loc[SAM.index.isin(cv), pv].values.sum()
+            io_df.loc[io_df.index == ck, pk] = SAM.loc[
+                SAM.index.isin(cv), pv
+            ].values.sum()
     # change from levesl to share (where each row sums to one)
     io_df = io_df.div(io_df.sum(axis=1), axis=0)
 
     return io_df
 
+
 import pandas as pd
 
 
-'''
+"""
 Create dictionaries to map micro categories to broad groups
 Codes come from https://www.wider.unu.edu/database/2015-social-accounting-matrix-south-africa
-'''
+"""
 CONS_DICT = {
-    "Food" : [
+    "Food": [
         "cagri",
         "clani",
         "cfore",
@@ -586,14 +596,14 @@ CONS_DICT = {
         "calcb",
         "csftd",
         "ctoba",
-],
+    ],
     "Energy and extraction": [
         "ccoal",
         "cmore",
         "comin",
         "celcg",
-        ],
-    "Non-durables" : [
+    ],
+    "Non-durables": [
         "ctexf",
         "ctexm",
         "ccarp",
@@ -625,7 +635,7 @@ CONS_DICT = {
         "cjewl",
         "comnf",
         "cwast",
-],
+    ],
     "Durables": [
         "cirst",
         "cnfme",
@@ -649,7 +659,7 @@ CONS_DICT = {
         "cairc",
         "coteq",
         "ccnst",
-        ],
+    ],
     "Services": [
         "ccsrv",
         "ctrad",
@@ -676,7 +686,7 @@ CONS_DICT = {
         "ceduc",
         "cheal",
         "cosrv",
-        ]
+    ],
 }
 PROD_DICT = {
     "Primary": [
@@ -690,7 +700,7 @@ PROD_DICT = {
     ],
     "Energy": [
         "aelcg",
-            ],
+    ],
     "Tertiary": [
         "awtrd",
         "artrd",
@@ -717,7 +727,7 @@ PROD_DICT = {
         "arecr",
         "aoact",
         "anobs",
-        ],
+    ],
     "Secondary Ex Energy": [
         "afood",
         "abevt",
@@ -750,5 +760,3 @@ PROD_DICT = {
         "acnst",
     ],
 }
-
-
--- ogzaf/input_output.py
+++ ogzaf/input_output.py
@@ -1,17 +1,23 @@
 import pandas as pd
 from ogzaf.constants import CON_DICT, PROD_DICT
 
-'''
+"""
 Read in SAM file
-'''
+"""
 # Read in SAM file
-storage_options = {'User-Agent': 'Mozilla/5.0'}
+storage_options = {"User-Agent": "Mozilla/5.0"}
 SAM_path = "https://www.wider.unu.edu/sites/default/files/Data/SASAM-2015-Data-Resource.xlsx"
-SAM = pd.read_excel(SAM_path, sheet_name="Micro SAM 2015", skiprows=6, index_col=0, storage_options=storage_options)
+SAM = pd.read_excel(
+    SAM_path,
+    sheet_name="Micro SAM 2015",
+    skiprows=6,
+    index_col=0,
+    storage_options=storage_options,
+)
 
 
 def get_alpha_c(SAM, cons_dict=CONS_DICT):
-    '''
+    """
     Calibrate the alpha_c vector, showing the shares of household
     expenditures for each consumption category
 
@@ -21,11 +27,14 @@ def get_alpha_c(SAM, cons_dict=CONS_DICT):
 
     Returns:
         alpha_c (dict): Dictionary of shares of household expenditures
-    '''
+    """
     overall_sum = 0
     for key, value in cons_dict.items():
         # note the subtraction of the row to focus on domestic consumption
-        categroy_total = SAM.loc[SAM.index.isin(value), "total"].sum() - SAM.loc[SAM.index.isin(value), "row"].sum()
+        categroy_total = (
+            SAM.loc[SAM.index.isin(value), "total"].sum()
+            - SAM.loc[SAM.index.isin(value), "row"].sum()
+        )
         alpha_c[key] = categroy_total
         overall_sum += categroy_total
     for key, value in cons_dict.items():
@@ -35,7 +44,7 @@ def get_alpha_c(SAM, cons_dict=CONS_DICT):
 
 
 def get_io_matrix(SAM, cons_dict=CONS_DICT, prod_dict=PROD_DICT):
-    '''
+    """
     Calibrate the io_matrix array.  This array relates the share of each
     production category in each consumption category
 
@@ -46,14 +55,12 @@ def get_io_matrix(SAM, cons_dict=CONS_DICT, prod_dict=PROD_DICT):
 
     Returns:
         io_df (pd.DataFrame): Dataframe of io_matrix
-    '''
+    """
     # Create initial matrix as dataframe of 0's to fill in
     io_dict = {}
     for key in prod_dict.keys():
         io_dict[key] = np.zeros(len(cons_dict.keys()))
-    io_df = pd.DataFrame(
-        io_dict, index=cons_dict.keys()
-    )
+    io_df = pd.DataFrame(io_dict, index=cons_dict.keys())
     # Fill in the matrix
     # Note, each cell in the SAM represents a payment from the columns
     # account to the row account
@@ -62,7 +69,9 @@ def get_io_matrix(SAM, cons_dict=CONS_DICT, prod_dict=PROD_DICT):
     # the production categories from columns
     for ck, cv in cons_dict.items():
         for pk, pv in prod_dict.items():
-            io_df.loc[io_df.index == ck, pk] = SAM.loc[SAM.index.isin(cv), pv].values.sum()
+            io_df.loc[io_df.index == ck, pk] = SAM.loc[
+                SAM.index.isin(cv), pv
+            ].values.sum()
     # change from levesl to share (where each row sums to one)
     io_df = io_df.div(io_df.sum(axis=1), axis=0)
 
