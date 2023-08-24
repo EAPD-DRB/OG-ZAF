83a7d186db7f7a6f25bea48b4c0fb93349b77414
jdebacker <jason.debacker@gmail.com>
2023-08-24 16:13:28 -0400

    temp changes

--- ogzaf/macro_params.py
+++ ogzaf/macro_params.py
@@ -11,7 +11,7 @@ import pandas as pd
 import numpy as np
 import datetime
 import statsmodels.api as sm
-from ogzaf import utils
+from ogzaf.utils import get_legacy_session
 from io import StringIO
 
 
