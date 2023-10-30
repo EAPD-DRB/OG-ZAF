7d2c1e6621e6f401b201ead3cbf40f3ca126274b
Richard Evans <rickecon@gmail.com>
2023-10-30 01:56:00 -0600

    Updated environment.yml and setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,32 @@
-import setuptools
+"""This file contains the OG-ZAF package's metadata and dependencies."""
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    longdesc = fh.read()
+from setuptools import find_packages, setup
 
-setuptools.setup(
+with open("README.md", "r") as readme_file:
+    readme = readme_file.read()
+
+setup(
     name="ogzaf",
-    version="0.0.0",
-    author="Marcelo LaFleur and Richard W. Evans",
+    version="0.0.1",
+    author="Marcelo LaFleur, Richard W. Evans, and Jason DeBacker",
     license="CC0 1.0 Universal (CC0 1.0) Public Domain Dedication",
     description="South Africa Calibration for OG-Core",
+    long_description=readme,
     long_description_content_type="text/markdown",
-    long_description=longdesc,
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "License :: OSI Approved :: Common Public License",
+        "Operating System :: POSIX",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Topic :: Scientific/Engineering :: Information Analysis",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    keywords="South Africa calibration of large scale overlapping generations model of fiscal policy",
     url="https://github.com/EAPD-DRB/OG-ZAF/",
     download_url="https://github.com/EAPD-DRB/OG-ZAF/",
     project_urls={
@@ -19,15 +35,15 @@ setuptools.setup(
     packages=["ogzaf"],
     package_data={
         "ogzaf": [
-            "ogzaf_default_parameters.json",
-            "data/*",
+            "ogusa_default_parameters.json",
+            "data/*"
         ]
     },
     include_packages=True,
     python_requires=">=3.7.7",
     install_requires=[
-        "psutil",
         "numpy",
+        "psutil",
         "scipy>=1.7.1",
         "pandas>=1.2.5",
         "matplotlib",
@@ -35,25 +51,14 @@ setuptools.setup(
         "distributed>=2.30.1",
         "paramtools>=0.15.0",
         "requests",
+        "pandas-datareader",
         "xlwt",
-        "openpyxl",
+        "openpyxl>=3.1.2",
         "statsmodels",
         "linearmodels",
         "black",
-        "pandas-datareader",
+        "linecheck",
         "ogcore",
-    ],
-    classifiers=[
-        "Development Status :: 2 - Pre-Alpha",
-        "Intended Audience :: Developers",
-        "Natural Language :: English",
-        "License :: CC0 1.0 Universal (CC0 1.0) Public Domain Dedication",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-        "Topic :: Software Development :: Libraries :: Python Modules",
     ],
     tests_require=["pytest"],
 )
