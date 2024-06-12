8d9442ec057f49fe616b5902b20432530b2959e4
jdebacker <jason.debacker@gmail.com>
2024-06-11 21:38:48 -0400

    format

--- tests/test_macro_params.py
+++ tests/test_macro_params.py
@@ -1,6 +1,7 @@
 """
 Tests of macro_params.py module
 """
+
 from ogzaf import macro_params
 
 
@@ -8,14 +9,17 @@ def test_get_macro_params():
     test_dict = macro_params.get_macro_params()
 
     assert isinstance(test_dict, dict)
-    assert list(test_dict.keys()).sort() == [
-        "r_gov_shift",
-        "r_gov_scale",
-        "alpha_T",
-        "alpha_G",
-        "initial_debt_ratio",
-        "g_y_annual",
-        "gamma",
-        "zeta_D",
-        "initial_foreign_debt_ratio"
-    ].sort()
+    assert (
+        list(test_dict.keys()).sort()
+        == [
+            "r_gov_shift",
+            "r_gov_scale",
+            "alpha_T",
+            "alpha_G",
+            "initial_debt_ratio",
+            "g_y_annual",
+            "gamma",
+            "zeta_D",
+            "initial_foreign_debt_ratio",
+        ].sort()
+    )
