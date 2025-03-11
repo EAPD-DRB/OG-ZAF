04f301072f8679d6069fed7bfe4c245596c3ba47
jdebacker <jason.debacker@gmail.com>
2025-03-11 14:38:44 -0400

    test coverage for when ping api and when not

--- tests/test_macro_params.py
+++ tests/test_macro_params.py
@@ -2,24 +2,36 @@
 Tests of macro_params.py module
 """
 
-from ogzaf import macro_params
+import pytest
+from ogidn import macro_params
 
 
-def test_get_macro_params():
-    test_dict = macro_params.get_macro_params()
+@pytest.mark.parametrize(
+    "update_from_api",
+    [True, False],
+    ids=["update_from_api=True", "update_from_api=False"],
+)
+def test_get_macro_params(update_from_api):
+    test_dict = macro_params.get_macro_params(update_from_api=update_from_api)
 
     assert isinstance(test_dict, dict)
-    assert (
-        list(test_dict.keys()).sort()
-        == [
-            "r_gov_shift",
-            "r_gov_scale",
-            "alpha_T",
-            "alpha_G",
-            "initial_debt_ratio",
-            "g_y_annual",
-            "gamma",
-            "zeta_D",
-            "initial_foreign_debt_ratio",
-        ].sort()
-    )
+    if update_from_api:
+        assert (
+            list(test_dict.keys()).sort()
+            == [
+                "r_gov_shift",
+                "r_gov_scale",
+                "alpha_T",
+                "alpha_G",
+                "initial_debt_ratio",
+                "g_y_annual",
+                "gamma",
+                "zeta_D",
+                "initial_foreign_debt_ratio",
+            ].sort()
+        )
+    else:
+        assert (
+            list(test_dict.keys()).sort()
+            == ["r_gov_shift", "r_gov_scale"].sort()
+        )
